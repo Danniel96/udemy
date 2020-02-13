c94eef6a4e2c5960a516509b53b2fd25a3ffd69f
Danniel <kisekae.danniel@gmail.com>
2020-02-13 09:46:57 +0800

    before Installing gulp and all

--- app/User.php
+++ app/User.php
@@ -12,7 +12,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+
+        'name', 
+        'email', 
+        'password',
+
     ];
 
     /**
@@ -21,6 +25,16 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+
+        'password', 
+        'remember_token',
+
     ];
+
+    public function role(){
+
+        return $this->belongsTo('App\Role');
+
+    }
+
 }
